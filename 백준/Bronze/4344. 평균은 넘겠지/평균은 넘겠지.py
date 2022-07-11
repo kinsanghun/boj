fb81e3e76bc672037566196f4388368f9cdf2f0a
KimSangHun <schmid_t@kakao.com>
2022-07-11 23:40:49 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for _ in range(n):
+    scores = list(map(int, input().split()))
+    avg = sum(scores[1:])/scores[0]
+    cnt = 0
+    for score in scores[1:]:
+        if score > avg:cnt += 1
+    rate = cnt/scores[0] *100
+    print(f'{rate:.3f}%')
