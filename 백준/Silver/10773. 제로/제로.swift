06062222693929aef2e190d4b46afb49c1454d06
KimSangHun <schmid_t@kakao.com>
2022-07-17 21:07:08 +0900

    [Silver IV] Title: 제로, Time: 56 ms, Memory: 81076 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.swift
+++ 백준/Silver/10773. 제로/제로.swift
@@ -5,10 +5,8 @@ var total:Int = 0
 var stack:[Int] = []
 for _ in 0..<cnt{
 	let n = Int(readLine()!)!
-	
-	if n == 0 {
-		total -= stack.popLast()!
-	} else {
+	if n == 0 {total -= stack.popLast()!} 
+    else {
 		stack.append(n)
 		total += n
 	}
