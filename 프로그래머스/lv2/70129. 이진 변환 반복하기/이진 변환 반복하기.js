a643d65b22e8920a565ad59e29d42951c4d0bb1b
KimSangHun <schmid_t@kakao.com>
2022-09-24 17:18:31 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.88 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,15 +1,11 @@
 function solution(s) {
-    var now = s;
-    var convert_count = 0;
-    var zero_count = 0;
-    while(now.length !== 1) {
-        convert_count++;
-        var arr = [...now].map(Number);
-        let one = arr.filter(n => n === 1).length
-        zero_count += arr.filter(n => n === 0).length
-        now = one.toString(2)
-        console.log(now)
+    var answer = [0, 0];
+    
+    while(s.length !== 1) {
+        var arr = [...s].map(Number);
+        answer[0]++;
+        answer[1] += arr.filter(n => n === 0).length;
+        s = arr.filter(n => n === 1).length.toString(2);
     }
-    var answer = [convert_count, zero_count];
     return answer;
 }
